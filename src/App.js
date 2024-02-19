298ea326e7c34ee4071ca49c335a2472f8b8fc2e
jassemjahangir <jassem.jahangir01@myhunter.cuny.edu>
2024-02-18 23:40:18 -0500

    react route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import logo from "./logo.svg";
-import "./App.css";
+import './App.css';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import Whatever from './pages/Whatever';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello World</h1>
-      </header>
-    </div>
+    <Router>
+      <div className='App'>
+        <header className='App-header'>
+          <Link to='/'>
+            <button>Home</button>
+          </Link>
+          <Link to='/Whatever'>
+            <button>Home2</button>
+          </Link>
+        </header>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/Whatever' element={<Whatever />} />
+        </Routes>
+        <footer className='App-footer'></footer>
+      </div>
+    </Router>
   );
 }
 
