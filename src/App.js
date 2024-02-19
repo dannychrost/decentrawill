f22a37608f7b5abb737b4441d9a679acfc481e7e
dannychrost <71196563+dannychrost@users.noreply.github.com>
2024-02-18 23:58:21 -0500

    Adding an h2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello World</h1>
-      </header>
-    </div>
-  );
+  return <h2>hello</h2>;
 }
 
 export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+// HomePage.js
+import React from "react";
+import "../styles/Home.css";
+
+function Home() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <p>Welcome to the home page!</p>
+    </div>
+  );
+}
+
+export default Home;
