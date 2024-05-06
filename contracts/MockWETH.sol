0d35b40b26dcd77e7239bc2a8b2f03029e40438c
dannychrost <71196563+dannychrost@users.noreply.github.com>
2024-05-06 04:02:32 -0400

    Minor changes

--- contracts/MockWETH.sol
+++ contracts/MockWETH.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract MockWETH is ERC20 {
     constructor() ERC20("Mock WETH", "mWETH") {
         _mint(
-            address(0xD71603C15CB21B0016EacE3626C8242c1E2eCb7C), // Deployer address, change to your preferred address
+            address(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266), // Deployer address, change to your preferred address
             10000 * 10 ** 18
         ); // Mint 10,000 mUSDC tokens to the deployer
     }
