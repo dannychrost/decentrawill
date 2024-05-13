3c9b7d80815238f70afd59aef0f7bf85db15e988
jassemjahangir <jassem.jahangir01@myhunter.cuny.edu>
2024-05-13 00:04:35 -0400

    seperated creator and beneficiary

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
-import Home from "./pages/Home";
-import Home2 from "./pages/Home2";
-import AppHome from "./pages/AppHome";
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import Home2 from './pages/Home2';
+import AppHome from './pages/AppHome';
+import BeneficiaryHome from './pages/BeneficiaryHome';
 
-import Faq from "./pages/Faq";
-import SignIn from "./pages/SignIn";
-import CreateAccount from "./pages/CreateAccount";
-import { Button } from "react-bootstrap";
-import { MainNavbar, AppNavbar, FooterNavbar } from "./components/Navbar";
-import { NavbarProvider, NavbarContext } from "./contexts/NavbarContext";
-import { WalletProvider } from "./contexts/WalletContext";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { AuthProvider } from "./contexts/AuthContext";
+import Faq from './pages/Faq';
+import SignIn from './pages/SignIn';
+import CreateAccount from './pages/CreateAccount';
+import { Button } from 'react-bootstrap';
+import { MainNavbar, AppNavbar, FooterNavbar } from './components/Navbar';
+import { NavbarProvider, NavbarContext } from './contexts/NavbarContext';
+import { WalletProvider } from './contexts/WalletContext';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { AuthProvider } from './contexts/AuthContext';
 function App() {
   return (
     <AuthProvider>
@@ -19,8 +20,8 @@ function App() {
         <Router>
           <NavbarProvider>
             <div
-              className="App"
-              style={{ display: "flex", flexDirection: "column" }}
+              className='App'
+              style={{ display: 'flex', flexDirection: 'column' }}
             >
               <div>
                 <NavbarContext.Consumer>
@@ -33,20 +34,24 @@ function App() {
               <div
                 style={{
                   background:
-                    "linear-gradient(315deg, #e056fd 0%, #000000 74%)",
-                  color: "white",
-                  padding: "10vw",
+                    'linear-gradient(315deg, #e056fd 0%, #000000 74%)',
+                  color: 'white',
+                  padding: '10vw',
 
-                  overflow: "hidden",
+                  overflow: 'hidden',
                 }}
               >
                 <Routes>
-                  <Route path="/" element={<Home />} />
-                  <Route path="/Home2" element={<Home2 />} />
-                  <Route path="/faq" element={<Faq />} />
-                  <Route path="/createAccount" element={<CreateAccount />} />
-                  <Route path="/signIn" element={<SignIn />} />
-                  <Route path="/appHome" element={<AppHome />} />
+                  <Route path='/' element={<Home />} />
+                  <Route path='/Home2' element={<Home2 />} />
+                  <Route path='/faq' element={<Faq />} />
+                  <Route path='/createAccount' element={<CreateAccount />} />
+                  <Route path='/signIn' element={<SignIn />} />
+                  <Route path='/appHome' element={<AppHome />} />
+                  <Route
+                    path='/beneficiaryHome'
+                    element={<BeneficiaryHome />}
+                  />
                 </Routes>
               </div>
               <div>
@@ -54,7 +59,7 @@ function App() {
               </div>
             </div>
           </NavbarProvider>
-        </Router>{" "}
+        </Router>{' '}
       </WalletProvider>
     </AuthProvider>
   );
