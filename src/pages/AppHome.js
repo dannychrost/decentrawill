76a83bb2c7211535d334ec8bb9c3c39e307ac734
dannychrost <71196563+dannychrost@users.noreply.github.com>
2024-05-13 00:44:48 -0400

    Just changed ticker symbols

--- src/pages/AppHome.js
+++ src/pages/AppHome.js
@@ -19,14 +19,14 @@ const tokens = [
     address: "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512", // Adding the address
   },
   {
-    symbol: "wETH",
+    symbol: "mWETH",
     imageUrl:
       "https://dynamic-assets.coinbase.com/dbb4b4983bde81309ddab83eb598358eb44375b930b94687ebe38bc22e52c3b2125258ffb8477a5ef22e33d6bd72e32a506c391caa13af64c00e46613c3e5806/asset_icons/4113b082d21cc5fab17fc8f2d19fb996165bcce635e6900f7fc2d57c4ef33ae9.png",
     name: "Ethereum",
     address: "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0", // Adding the address
   },
   {
-    symbol: "wBTC",
+    symbol: "mWBTC",
     imageUrl:
       "https://dynamic-assets.coinbase.com/e785e0181f1a23a30d9476038d9be91e9f6c63959b538eabbc51a1abc8898940383291eede695c3b8dfaa1829a9b57f5a2d0a16b0523580346c6b8fab67af14b/asset_icons/b57ac673f06a4b0338a596817eb0a50ce16e2059f327dc117744449a47915cb2.png",
     name: "Bitcoin",
